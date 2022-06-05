3c4450370ba5e8b3a27510dc5988949f2576b4ea
swapnil.waghmare24 <iamswapnilengg@gmail.com>
2022-06-05 16:03:56 +0530

    last name validated

--- main/java/UserValidation.java
+++ main/java/UserValidation.java
@@ -17,9 +17,25 @@ public class UserValidation {
         else
             System.out.println("First Name is Invalid");
     }
+    public void lastName() {
+
+        String lastNamePattern = "[A-Z]{1}[a-z]{2,}";
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Last Name:");
+        String lastName = scanner.next();
+        Pattern pattern = Pattern.compile(lastNamePattern);
+        Matcher matcher = pattern.matcher(lastName);
+        boolean result = matcher.matches();
+
+        if (result == true)
+            System.out.println("Last Name is Valid");
+        else
+            System.out.println("Last Name is Invalid");
+    }
 
     public static void main(String[] args) {
     UserValidation userValidation=new UserValidation();
     userValidation.firstName();
+    userValidation.lastName();
     }
 }
